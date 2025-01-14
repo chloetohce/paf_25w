940e3bc03d17f30ff03981c0471d383228a35d7e
chloetohce <chloetohce@gmail.com>
2025-01-14 14:35:29 +0800

    Init

---
+++ src/main/java/paf/workshop/paf_24w/Paf24wApplication.java
@@ -0,0 +1,13 @@
+package paf.workshop.paf_24w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf24wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf24wApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paf/workshop/paf_24w/Paf24wApplicationTests.java
@@ -0,0 +1,13 @@
+package paf.workshop.paf_24w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf24wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
